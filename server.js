54bd8262a891bfd67dd6a43309a17f862ff25307
Dmitrii Kechin <dmitrii.kechin@gmail.com>
2022-08-12 15:20:37 +0300

    bug fixed

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const path = require('path');
 const PORT = process.env.PORT || 8080;
 
 const app = express();
-app.use = express();
 app.use(express.static(__dirname));
 app.use(express.static(path.resolve(__dirname, 'build')));
 
